97dce4042382a854c5f9ecc718be50e5584a57d5
Vitalii <vitalii.skorokhod@gmail.com>
2016-01-30 03:42:01 -0600

    Drop support for old IE versions

--- header.php
+++ header.php
@@ -7,15 +7,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php // add support for html5 elements in older IE versions ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-<![endif]-->
-
 <?php wp_head(); // wp_head() should be just before the closing </head> tag, or many plugins will be broken ?>
 
 </head>
-
 
 <body <?php body_class(); ?>>
 
