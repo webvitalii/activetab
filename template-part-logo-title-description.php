4f1f1686deccc07e5e54b5dba4a57b7ab5a0ef14
Vitalii <vitalii.skorokhod@gmail.com>
2016-01-30 03:29:32 -0600

    Remove options completely
    Remove Options Framework

--- template-part-logo-title-description.php
+++ template-part-logo-title-description.php
@@ -1,11 +1,5 @@
 
 <?php
-$logo_url = of_get_option( 'logo_url', '' );
-
-$title_desc = esc_attr( get_bloginfo( 'name', 'display' ) );
-if ( get_bloginfo( 'description' ) ) { // add desc to title attr
-	$title_desc .= ' | '.esc_attr( get_bloginfo( 'description', 'display' ) );
-}
 
 if ( activetab_is_homepage() ) {
 	$link_before = '';
@@ -17,20 +11,11 @@ if ( activetab_is_homepage() ) {
 ?>
 
 	<header id="masthead" class="site-header clearfix" role="banner">
-		<div class="site-logo-title clearfix">
-			<?php if ( ! empty( $logo_url ) ) : ?>
-				<div class="site-logo">
-					<?php echo $link_before; ?><img src="<?php echo $logo_url; ?>" alt="<?php echo $title_desc; ?>"><?php echo $link_after; ?>
-				</div><!-- .site-logo -->
-			<?php endif; ?>
 
-			<div class="site-title-description">
-				<h3 class="site-title"><?php echo $link_before; ?><?php bloginfo( 'name' ); ?><?php echo $link_after; ?></h3>
+		<h3 class="site-title"><?php echo $link_before; ?><?php bloginfo( 'name' ); ?><?php echo $link_after; ?></h3>
 
-				<?php if ( get_bloginfo( 'description' ) ) : ?>
-				<h4 class="site-description muted"><?php bloginfo( 'description' ); ?></h4>
-				<?php endif; ?>
-			</div><!-- .site-title-description -->
+		<?php if ( get_bloginfo( 'description' ) ) : ?>
+		<h4 class="site-description text-muted"><?php bloginfo( 'description' ); ?></h4>
+		<?php endif; ?>
 
-		</div><!-- .site-logo-title -->
 	</header><!-- #masthead .site-header -->
