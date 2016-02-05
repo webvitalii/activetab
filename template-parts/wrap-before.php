050090fbf9b25db88c34d3047c01fab75430758a
Vitalii <vitalii.skorokhod@gmail.com>
2016-02-05 16:55:22 -0600

    Added logo to site header

--- template-parts/wrap-before.php
+++ template-parts/wrap-before.php
@@ -6,6 +6,8 @@ $col_class = 'col-sm-6';
 
 $settings = activetab_get_settings();
 
+$logo_url = $settings['logo_url'];
+
 if( $settings['layout'] == 'sidebar-content' || $settings['layout'] == 'content-sidebar' ) {
 	$col_class = 'col-sm-9';
 }
@@ -23,6 +25,11 @@ if( is_page_template ( 'template-full-width-no-sidebar.php' ) ){ // show wide co
 
 
 <?php
+$title_desc = esc_attr( get_bloginfo( 'name', 'display' ) );
+if ( get_bloginfo( 'description' ) ) { // add desc to title attr
+	$title_desc .= ' | '.esc_attr( get_bloginfo( 'description', 'display' ) );
+}
+ 
 if ( activetab_is_homepage() ) {
 	$link_before = '';
 	$link_after = '';
@@ -34,6 +41,12 @@ if ( activetab_is_homepage() ) {
 
 				<header class="site-header clearfix" role="banner">
 
+					<?php if ( ! empty( $logo_url ) ) : ?>
+					<div class="site-logo">
+						<?php echo $link_before; ?><img src="<?php echo $logo_url; ?>" alt="<?php echo $title_desc; ?>"><?php echo $link_after; ?>
+					</div><!-- .site-logo -->
+					<?php endif; ?>
+				
 					<h3 class="site-title"><?php echo $link_before; ?><?php bloginfo( 'name' ); ?><?php echo $link_after; ?></h3>
 
 					<?php if ( get_bloginfo( 'description' ) ) : ?>
