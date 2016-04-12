d0e64e572788b6877e1a5e6c571e37754ab47d66
Vitalii <vitalii.skorokhod@gmail.com>
2016-04-12 14:48:29 -0500

    Added functions.php to child sample theme
    Added functions.php to child sample theme with sample code to remove
    parent styles and scripts

---
+++ activetab-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+function activetab_remove_scripts() {
+    wp_dequeue_style( 'activetab-bootstrap-theme-style' );
+    wp_deregister_style( 'activetab-bootstrap-theme-style' );
+
+    wp_dequeue_script( 'activetab-script' );
+    wp_deregister_script( 'activetab-script' );
+}
+add_action( 'wp_enqueue_scripts', 'activetab_remove_scripts', 20 );
+*/
